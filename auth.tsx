27924a38f5eaa78056a117c866d214a012948fde
aeum1016 <aeum1016@gmail.com>
2024-11-29 13:39:07 -0500

    Figured out the refresh token

--- auth.tsx
+++ auth.tsx
@@ -37,8 +37,8 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
             "https://www.googleapis.com/auth/userinfo.email",
             "https://www.googleapis.com/auth/userinfo.profile",
           ].join(" "),
+          prompt: "consent",
           access_type: "offline",
-          response_type: "code",
         },
       }
     }),
@@ -53,4 +53,51 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async session({ session, user }) {
+      const googleAccount = await pool.query(`SELECT * FROM public.accounts WHERE "userId" = $1::integer AND "provider" = 'google'`, [user.id])
+      if (googleAccount.rowCount !== null && googleAccount.rows[0].expires_at * 1000 < Date.now()) {
+        // If the access token has expired, try to refresh it
+        try {
+          // https://accounts.google.com/.well-known/openid-configuration
+          // We need the `token_endpoint`.
+          const response = await fetch("https://oauth2.googleapis.com/token", {
+            method: "POST",
+            body: new URLSearchParams({
+              client_id: process.env.AUTH_GOOGLE_ID!,
+              client_secret: process.env.AUTH_GOOGLE_SECRET!,
+              grant_type: "refresh_token",
+              refresh_token: googleAccount.rows[0].refresh_token,
+            }),
+          })
+
+          const tokensOrError = await response.json()
+
+          if (!response.ok) throw tokensOrError
+
+          const newTokens = tokensOrError as {
+            access_token: string
+            expires_in: number
+            refresh_token?: string
+          }
+
+          await pool.query(`UPDATE public.accounts 
+            SET (access_token, expires_at, refresh_token) = ($1::text, $2::bigint, $3::text) 
+            WHERE "provider"='google' AND "providerAccountId" = $4::text`,
+            [newTokens.access_token, Math.floor(Date.now() / 1000 + newTokens.expires_in), newTokens.refresh_token ?? googleAccount.rows[0].refresh_token, googleAccount.rows[0].providerAccountId])
+        } catch (error) {
+          console.error("Error refreshing access_token", error)
+          // If we fail to refresh the token, return an error so we can handle it on the page
+          session.error = "RefreshTokenError"
+        }
+      }
+      return session
+    },
+  }
 });
+
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshTokenError"
+  }
+}
